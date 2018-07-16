cc8674abc0677f1613ee9728a93eaffe2717da3a
ChandraMohan <chandra.nair@ust-global.com>
2018-07-16 16:58:58 +0530

    initial

---
+++ CMakeCythonIntegration-master/CMakeCythonIntegration-master.cmake
@@ -0,0 +1,11 @@
+	cmake_minimum_required(VERSION 2.6)
+	add_executable(	Adder src/Adder.cpp	)
+	add_executable(	Adderh src/Adder.hpp	)
+	add_executable(	StdoutWriter src/StdoutWriter.cpp	)
+	add_executable(	StdoutWriterh src/StdoutWriter.hpp	)
+	add_executable(	AdderTests test/AdderTests.cpp	)
+	add_executable(	StdoutWriterTest test/StdoutWriterTest.cpp	)
+
+
+
+
